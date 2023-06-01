5af33b1ed2d2363e73c5d17a9dfbb089e213fccf
Pawel Wieczorek <p.wieczorek@samsung.com>
2023-06-01 11:57:52 +0200

    Update UnflattenLib use in --dynamic-init mode
    Pull request kflat#81 changed the naming convention used in Unflatten
    library. This commit applies all the necessary changes to ensure that
    off-targets build with --dynamic-init works on the newest version of
    this lib.

--- src/resources/dyn_init.c
+++ src/resources/dyn_init.c
@@ -20,22 +20,22 @@ void initialize_function_pointer_stubs(void);
 void aot_kflat_initialize_global_variables(void);
 void* fptrstub_search(const char* symbol);
 
-/* Just a simple wrapper to match function prototype with the one expected by flatten_load*/
+/* Just a simple wrapper to match function prototype with the one expected by unflatten_load*/
 static uintptr_t get_fpointer_test_function_address(const char* fsym) {
 	return (uintptr_t) fptrstub_search(fsym);
 }
 
 /* KFLAT library wrappers */
-static CFlatten flatten = NULL;
+static CUnflatten unflatten = NULL;
 
 void aot_kflat_init(const char* imgpath) {
 	int ret;
 
-	if(flatten != NULL) {
+	if(unflatten != NULL) {
 		fprintf(stderr, "Stay where you are! For some reason aot_kflat_init was invoked twice.\n");
 		fprintf(stderr, "Such scenario is impossible to happen in non-modified off-target!\n");
 		fprintf(stderr, "\nWhatever you're doing, please remember to call aot_kflat_fini!!\n");
-		assert(flatten != NULL);
+		assert(unflatten != NULL);
 	}
 
 	FILE* in = fopen(imgpath, "r");
@@ -50,10 +50,10 @@ void aot_kflat_init(const char* imgpath) {
 	/* Setup function pointers now, as they are required for loading kflat image */
 	initialize_function_pointer_stubs();
 
-	flatten = flatten_init(0);
-	assert(flatten != NULL);
+	unflatten = unflatten_init(0);
+	assert(unflatten != NULL);
 
-	ret = flatten_load(flatten, in, get_fpointer_test_function_address);
+	ret = unflatten_load(unflatten, in, get_fpointer_test_function_address);
 	assert(ret == 0);
 
 	/* Setup globals after flattened image with their content has been loaded */
@@ -63,10 +63,10 @@ void aot_kflat_init(const char* imgpath) {
 }
 
 void aot_kflat_fini(void) {
-	flatten_deinit(flatten);
-	flatten = NULL;
+	unflatten_deinit(unflatten);
+	unflatten = NULL;
 }
 
 void* aot_kflat_root_by_name(const char* name, unsigned long* size) {
-	return flatten_root_pointer_named(flatten, name, size);
+	return unflatten_root_pointer_named(unflatten, name, size);
 }
