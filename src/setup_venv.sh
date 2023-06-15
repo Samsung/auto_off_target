723035f6156abaf826ff80e2b742bb1c99fe01ff
Tomasz Kuchta <t.kuchta@samsung.com>
2023-06-15 10:03:10 +0200

    address review feedback

--- src/setup_venv.sh
+++ src/setup_venv.sh
@@ -5,13 +5,13 @@
 # Copyright Samsung Electronics
 # Samsung Mobile Security Team @ Samsung R&D Poland
  
-echo -e "1. Running python venv module\n"
-python3 -m venv venv            || echo "Failed to load the venv module"
+echo -e "1. Creating virtual environment.\n"
+python3 -m venv venv            || echo "Failed to load the venv module."
 
-echo -e "2. Running activate\n"
-source venv/bin/activate        || echo "Failed to activate venv"
+echo -e "2. Activating virtual environment.\n"
+source venv/bin/activate        || echo "Failed to activate venv."
 
-echo -e "3. Installing requirements\n"
-pip install -r requirements.txt || echo "Failed to install requirements"
+echo -e "3. Installing requirements.\n"
+pip install -r requirements.txt || echo "Failed to install requirements."
 
-echo -e "\n4. All good! You can now run. To exit -> enter 'deactivate'\n" 
+echo -e "\n4. All good! You are now within the virtual environment. Run the 'deactivate' command to exit.\n" 
