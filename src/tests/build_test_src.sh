45ead9036465211f03785721cda251587ab758f2
Blazej Jaworowski <b.jaworowski@samsung.com>
2023-11-22 12:34:00 +0100

    Add github actions test dependency build

--- src/tests/build_test_src.sh
+++ src/tests/build_test_src.sh
@@ -7,12 +7,6 @@
  
 set -u
 
-if [ -z $CAS_DIR ]
-then
-    echo "Make sure CAS_DIR is set correctly"
-    exit 1
-fi
-
 (cd test_data/src/csmith_test && etrace -w etrace_data make) ||
     { echo "csmith_test build failed" ; exit 2; }
 (cd test_data/src/tinycc && ./configure && etrace -w etrace_data make) ||
