96dac27647d752f5deb98b10687322a389e2dc72
Blazej Jaworowski <b.jaworowski@samsung.com>
2023-11-24 12:56:29 +0100

    Isolate test case runs

--- src/tests/test_e2e.py
+++ src/tests/test_e2e.py
@@ -40,11 +40,9 @@ class TestE2E(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
-        self.cwd_path = os.getcwd()
-
         self.keep_test_env = False
         if 'KEEP_TEST_ENV' in os.environ:
-            self.keep_test_env = os.environ['KEEP_TEST_ENV'] == 'True'
+            self.keep_test_env = os.environ['KEEP_TEST_ENV'].lower() == 'true'
 
         test_data_path = os.path.join(os.path.dirname(__file__), 'test_data')
         tinycc_config = os.path.join(test_data_path, 'test_config_tinycc.json')
@@ -55,14 +53,11 @@ class TestE2E(unittest.TestCase):
 
         if 'AOT_REGRESSION_PATH' not in os.environ:
             self.fail('Make sure AOT_REGRESSION_PATH is set')
-        regression_aot_path = os.environ['AOT_REGRESSION_PATH']
+        self.regression_aot_path = os.environ['AOT_REGRESSION_PATH']
 
-        timeout = None
+        self.timeout = None
         if 'AOT_TIMEOUT' in os.environ:
             self.timeout = int(os.environ['AOT_TIMEOUT'])
-
-        self.tester = RegressionTester(regression_aot_path, timeout,
-                                       generate_run_scripts=self.keep_test_env)
 
         self.test_configs = []
         for test_config in test_configs.split():
@@ -71,36 +66,6 @@ class TestE2E(unittest.TestCase):
                 config = TestE2E.Config(**data)
                 config.name = test_config
                 self.test_configs.append(config)
-
-    @classmethod
-    def tearDownClass(self):
-        os.chdir(self.cwd_path)
-
-    def set_up_test_case(self, i, data_dir):
-        execution_dir_name = None
-        if self.keep_test_env:
-            execution_dir_name = os.path.join(os.path.dirname(__file__),
-                                              'test_env',
-                                              f'test{i}_{uuid.uuid4()}')
-            try:
-                os.makedirs(execution_dir_name)
-            except OSError:
-                pass
-        else:
-            self.temp_dir = tempfile.TemporaryDirectory()
-            execution_dir_name = self.temp_dir.name
-
-        os.chdir(execution_dir_name)
-        print(f'Working in directory: {execution_dir_name}')
-
-        data_dir = os.path.join(os.path.dirname(__file__), 'test_data', data_dir)
-        shutil.copytree(data_dir, execution_dir_name, dirs_exist_ok=True)
-
-        return execution_dir_name
-
-    def clean_up_test_case(self):
-        if not self.keep_test_env:
-            self.temp_dir.cleanup()
 
     def _prepare_options(test_config, function, case):
         options = {
@@ -116,12 +81,39 @@ class TestE2E(unittest.TestCase):
             options[k] = v
         return options
 
+    def _run_test_case(self, test_config, function, i, case):
+        # setup test env
+        temp_dir = None
+        if self.keep_test_env:
+            execution_dir_name = os.path.join(os.path.dirname(__file__),
+                                              'test_env',
+                                              f'test{i}_{uuid.uuid4()}')
+            os.makedirs(execution_dir_name, exist_ok=True)
+        else:
+            temp_dir = tempfile.TemporaryDirectory()
+            execution_dir_name = temp_dir.name
+
+        original_cwd = os.getcwd()
+        os.chdir(execution_dir_name)
+        print(f'Working in directory: {execution_dir_name}')
+
+        data_dir = os.path.join(os.path.dirname(__file__), 'test_data', test_config.data_dir)
+        shutil.copytree(data_dir, execution_dir_name, dirs_exist_ok=True)
+
+        # test
+        with self.subTest(f'Test {test_config.name} ({function}) [{i}] at {execution_dir_name}'):
+            tester = RegressionTester(self.regression_aot_path, self.timeout,
+                                      generate_run_scripts=self.keep_test_env)
+            options = TestE2E._prepare_options(test_config, function, case)
+            tester.run_regression(self, options, case.build_offtarget)
+
+        # cleanup test env
+        os.chdir(original_cwd)
+        if not self.keep_test_env:
+            temp_dir.cleanup()
+
     def test_regression(self):
         for test_config in self.test_configs:
             for function in test_config.source.functions:
                 for i, case in enumerate(test_config.cases):
-                    execution_dir_name = self.set_up_test_case(i, test_config.data_dir)
-                    options = TestE2E._prepare_options(test_config, function, case)
-                    with self.subTest(f'Test {test_config.name} ({function}) [{i}] at {execution_dir_name}'):
-                        self.tester.run_regression(self, options, case.build_offtarget)
-                    self.clean_up_test_case()
+                    self._run_test_case(test_config, function, i, case)
